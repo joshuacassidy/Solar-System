da110acc1eb3ac3733c4c9d22cac0e799a3d4214
joshuacassidy <Joshcassidy79@gmail.com>
2017-09-09 23:01:15 +0100

    Added File Writer to write the planet and moon data to a file

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
@@ -6,7 +8,7 @@ public class Main {
     private static Set<CelestialBody> planets = new HashSet<>();
     private static Set<CelestialBody> moons = new HashSet<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String[] planetNames = {"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto"};
         int[] planetOrbitalPeriods = {88,225,365,687,4332,10759,30660,165,248};
@@ -15,11 +17,20 @@ public class Main {
         double[] moonOrbitalPeriods = {27,1.3,0.3,1.8,3.5,7.1,16.7};
         CelestialBody temp;
         CelestialBody tempMoon;
+        
+        try(FileWriter planetsFile = new FileWriter("src/Planets.txt");
+            FileWriter TransactionsFile = new FileWriter("src/Moon.txt")) {
+                for(int i = 0; i< planetNames.length; i++) {
+                    planetsFile.write(String.format("%s, %s\n",planetNames[i],planetOrbitalPeriods[i]));
+                }
+                for(int i = 0; i< moonNames.length; i++) {
+                TransactionsFile.write(String.format("%s, %s\n",moonNames[i], moonOrbitalPeriods[i]));
+                }
+        }
 
         for(int i = 0; i < planetNames.length; i++){
             temp = new Planet(planetNames[i],planetOrbitalPeriods[i]);
             addValues(temp,planets);
-
         }
 
         for(int i = 0; i < moonNames.length; i++){
