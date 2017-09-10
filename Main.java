15e7a97658395b98ccfe547ba10d5ac9dcbd6deb
joshuacassidy <Joshcassidy79@gmail.com>
2017-09-10 13:42:06 +0100

    added code to read data back from binary files

--- Main.java
+++ Main.java
@@ -9,11 +9,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException,ClassNotFoundException {
         //No longer needed to run program
-        String[] planetNames = {"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto"};
-        int[] planetOrbitalPeriods = {88,225,365,687,4332,10759,30660,165,248};
-
-        String[] moonNames = {"Moon","Deimos","Phobos","Io","Europa","Ganymede","Callisto"};
-        double[] moonOrbitalPeriods = {27,1.3,0.3,1.8,3.5,7.1,16.7};
+//        String[] planetNames = {"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto"};
+//        int[] planetOrbitalPeriods = {88,225,365,687,4332,10759,30660,165,248};
+//
+//        String[] moonNames = {"Moon","Deimos","Phobos","Io","Europa","Ganymede","Callisto"};
+//        double[] moonOrbitalPeriods = {27,1.3,0.3,1.8,3.5,7.1,16.7};
         CelestialBody temp;
         CelestialBody tempMoon;
 
@@ -51,17 +51,47 @@ public class Main {
 //            }
 //        }
 
-        try (ObjectOutputStream planetsFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Planets.dat"))); ObjectOutputStream moonsFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Moons.dat")))) {
+//        try (ObjectOutputStream planetsFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Planets.dat"))); ObjectOutputStream moonsFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Moons.dat")))) {
+//
+//            for(int i = 0; i < planetNames.length; i++) {
+//                    planetsFile.writeObject(String.format("%s, %s\n",planetNames[i],planetOrbitalPeriods[i]));
+//                }
+//                for(int i = 0; i< moonNames.length; i++) {
+//                    moonsFile.writeObject(String.format("%s, %s\n",moonNames[i], moonOrbitalPeriods[i]));
+//                }
+//        }
 
-            for(int i = 0; i < planetNames.length; i++) {
-                    planetsFile.writeObject(String.format("%s, %s\n",planetNames[i],planetOrbitalPeriods[i]));
+        try(ObjectInputStream planetsFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("src/Planets.dat"))); ObjectInputStream moonsFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("src/Moons.dat")))) {
+            boolean planetEof = false;
+
+            while(!planetEof) {
+                try {
+                    Object planetData = planetsFile.readObject();
+                    String[] data = planetData.toString().split(",");
+                    temp = new Planet(data[0], Double.parseDouble(data[1]));
+                    addValues(temp, planets);
+                } catch (EOFException e) {
+                    planetEof = true;
+
                 }
-                for(int i = 0; i< moonNames.length; i++) {
-                    moonsFile.writeObject(String.format("%s, %s\n",moonNames[i], moonOrbitalPeriods[i]));
+            }
+
+
+                boolean moonsEof = false;
+                while(!moonsEof) {
+                    try {
+                        Object moonData = moonsFile.readObject();
+                        String[] data = moonData.toString().split(",");
+                        tempMoon = new Moon(data[0], Double.parseDouble(data[1]));
+                        addValues(tempMoon, moons);
+
+
+                    } catch (EOFException e) {
+                        moonsEof = true;
+
                 }
+            }
         }
-
-
 
 
         printCelestialBody(moons,"Moons in this solar system: ");
